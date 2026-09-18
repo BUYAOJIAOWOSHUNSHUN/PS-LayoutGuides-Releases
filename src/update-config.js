cd77c2940357d9a3dd863f07e44aec1fa9f98e6f
SHUNx2 <327629258+BUYAOJIAOWOSHUNSHUN@users.noreply.github.com>
2026-09-19 01:12:10 +0800

    上传源码：src/update-config.js

--- src/update-config.js
+++ src/update-config.js
@@ -13,6 +13,6 @@
 const REPO = "BUYAOJIAOWOSHUNSHUN/PS-LayoutGuides-Releases";
 const SUBDIR = "";
 const REF_OVERRIDE = "";
-const VERSION = "1.9.15";
+const VERSION = "1.9.16";
 
 module.exports = { REPO, SUBDIR, REF_OVERRIDE, VERSION };
